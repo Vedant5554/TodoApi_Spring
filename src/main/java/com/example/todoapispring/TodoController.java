ea0a23456465579895f91deee8fb4dd3b29ffcd3
Vedant5554 <vgavate3011@gmail.com>
2026-01-20 12:22:56 +0530

    returning not found request in json format

--- src/main/java/com/example/todoapispring/TodoController.java
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class TodoController {
 
     private static List<Todo> todoList;
+    private static final String todo_not_found = "todo not found";
     public TodoController(){
         todoList = new ArrayList<>();
         todoList.add(new Todo(1,false,"Todo 1",1));
@@ -43,14 +44,26 @@ public class TodoController {
         return newTodo;
     }
 
-    @GetMapping("/todos/{todoId}")
-    public ResponseEntity<Todo> getTodoById(@PathVariable long todoId){
+//    @GetMapping("/todos/{todoId}")
+//    public ResponseEntity<Todo> getTodoById(@PathVariable long todoId){
+//        for(Todo todo : todoList){
+//            if(todo.getId() == todoId){
+//                return ResponseEntity.ok(todo);
+//            }
+//        }
+//        return ResponseEntity.notFound().build(); //instead of returning 404 not found we can return a json object todo not found that is the hw
+//    }
+
+
+    //HW 1 done
+    @GetMapping("/{todoId}")
+    public ResponseEntity<?> getTodoById(@PathVariable long todoId){
         for(Todo todo : todoList){
             if(todo.getId() == todoId){
                 return ResponseEntity.ok(todo);
             }
         }
-        return ResponseEntity.notFound().build(); //instead of returning 404 not found we can return a json object todo not found that is the hw
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(todo_not_found);
     }
 
 //hw
@@ -58,6 +71,8 @@ public class TodoController {
     //Implements the api & figure out the api contract also for the following
     //delete request for deleting a todo
     //patch request to update a specific todo
+
+
 
 
 
