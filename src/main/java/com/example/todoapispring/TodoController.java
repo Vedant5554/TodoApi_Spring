3d734fc41b70b7602038ab24ff79c84fd03cd5d4
Vedant5554 <vgavate3011@gmail.com>
2026-01-21 10:06:34 +0530

    Added use of qualifier component and service and some changes in the main code

--- src/main/java/com/example/todoapispring/TodoController.java
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -1,5 +1,7 @@
 package com.example.todoapispring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,96 +10,107 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/todos") //no need to add todos infront of other getmaping
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
+    private TodoService todoService; // anotherTodoService
+
+    private TodoService todoservice2; // fakeTodoService
+
     private static List<Todo> todoList;
-    private static final String todo_not_found = "todo not found";
-    public TodoController(){
+    // Error message when the todo is not found
+    private static final String TODO_NOT_FOUND = "Todo not found";
+
+    public TodoController(
+            @Qualifier("anotherTodoService") TodoService todoService,
+            @Qualifier("fakeTodoService")  TodoService todoservice2) {
+
+        this.todoService = todoService;
+        this.todoservice2 = todoservice2;
         todoList = new ArrayList<>();
-        todoList.add(new Todo(1,false,"Todo 1",1));
-        todoList.add(new Todo(2,true,"Todo 2",2));
+        todoList.add(new Todo(1, false, "Todo 1", 1));
+        todoList.add(new Todo(2, true, "Todo 2", 2));
     }
 
 
-    @GetMapping()
-    public ResponseEntity<List<Todo>> getTodos(){
+    @GetMapping
+    public ResponseEntity<List<Todo>> getTodos(@RequestParam(required = false) Boolean isCompleted) {
+        System.out.println("Incoming query params: " + isCompleted + " " + this.todoservice2.doSomething());
         return ResponseEntity.ok(todoList);
     }
 
-    @PostMapping()
+    @PostMapping
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
 
-    public Todo createTodo(@RequestBody Todo newTodo){
-
-        /*
-         * @ResponseStatus(HttpStatus.CREATED) we can set this annotation to set the status code
+        /**
+         * we can use this annotation to set the status code @ResponseStatus(HttpStatus.CREATED)
          *
-         * another way for this is
-         *
-         * public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo){
-         * todoList.add(newTodo);
-         * return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
-         * }
-         * this helps to handle manually
          */
         todoList.add(newTodo);
-        return newTodo;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
     }
 
-//    @GetMapping("/todos/{todoId}")
-//    public ResponseEntity<Todo> getTodoById(@PathVariable long todoId){
-//        for(Todo todo : todoList){
-//            if(todo.getId() == todoId){
-//                return ResponseEntity.ok(todo);
-//            }
-//        }
-//        return ResponseEntity.notFound().build(); //instead of returning 404 not found we can return a json object todo not found that is the hw
-//    }
 
-
-    //HW 1 done
     @GetMapping("/{todoId}")
-    public ResponseEntity<?> getTodoById(@PathVariable long todoId){
-        for(Todo todo : todoList){
-            if(todo.getId() == todoId){
+    public ResponseEntity<?> getTodoById(@PathVariable Long todoId) {
+        for (Todo todo : todoList) {
+            if (todo.getId() == todoId) {
                 return ResponseEntity.ok(todo);
             }
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(todo_not_found);
+        // HW: Along with 404 status code, try to send a json {message: Todo not found}
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
     }
 
-//hw
-    //figure out how we can send error json response with `ResponseEntity` for not found cases
-    //Implements the api & figure out the api contract also for the following
-    //delete request for deleting a todo
-    //patch request to update a specific todo
-
-
-//    @DeleteMapping
-//    public ResponseEntity<Void> deleteTodos(){
-//        todoList.clear();
-//        return ResponseEntity.noContent().build(); //this will delete everything in that list
-//    }
-//
-
+    /**
+     * API to delete a Todo
+     * We can delete a particular todo using its ID as it is unique for every todo.
+     */
     @DeleteMapping("/{todoId}")
-    public ResponseEntity<Void> deleteTodos(@PathVariable long todoId){
-//        for (Todo todo : todoList ){
-//            if(todo.getId() == todoId){
-//                todoList.remove(todo);
-//            }
-//        }//wrong
-
-        //correct
-        boolean removed = todoList.removeIf(todo -> todo.getId() == todoId);
-        if (!removed) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<?> deleteTodoById(@PathVariable Long todoId) {
+        Todo todoToRemove = null;
+        for(Todo todo : todoList) {
+            if(todo.getId() == todoId) {
+                todoToRemove = todo;
+                break;
+            }
         }
 
-        return ResponseEntity.noContent().build();
-
-
+        if(todoToRemove != null) {
+            todoList.remove(todoToRemove);
+            String deleteSuccessMessage = "Todo deleted successfully";
+            return ResponseEntity.status(HttpStatus.OK).body(deleteSuccessMessage);
+        } else {
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+        }
     }
 
+    /**
+     * API to update an existing Todo using its ID
+     * @param todoId
+     * @param title
+     * @param isCompleted
+     * @param userId
+     * @return
+     */
+    @PatchMapping("/{todoId}")
+    ResponseEntity<?> updateTodoById(@PathVariable Long todoId, @RequestParam(required = false) String title, @RequestParam(required = false) Boolean isCompleted, Integer userId) {
+        for(Todo todo : todoList) {
+            if(todo.getId() == todoId) {
+                if(title != null) {
+                    todo.setTitle(title);
+                }
+                if(isCompleted != null) {
+                    todo.setCompleted(isCompleted);
+                }
+                if(userId != null) {
+                    todo.setUserId(userId);
+                }
+
+                return ResponseEntity.ok(todo);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+    }
 
 }
