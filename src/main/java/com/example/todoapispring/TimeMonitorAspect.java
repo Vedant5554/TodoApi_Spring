e8bca32daa980da5c3b0c1a5d2f1e202448e5b5f
Vedant5554 <vgavate3011@gmail.com>
2026-02-05 22:23:49 +0530

    Added SLF4J LOGGER

--- src/main/java/com/example/todoapispring/TimeMonitorAspect.java
+++ src/main/java/com/example/todoapispring/TimeMonitorAspect.java
@@ -6,15 +6,21 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger; // imp for logger
+import org.slf4j.LoggerFactory; //imp for logger
 
-import java.sql.SQLOutput;
 
 @Aspect
 @Component
 
 public class TimeMonitorAspect {
+    private static final Logger logger =
+            LoggerFactory.getLogger(TimeMonitorAspect.class); //slf4j
+
     @Around("@annotation(TimeMonitor)")
     public void logtime(ProceedingJoinPoint joinPoint){
+
+
         //business logic
         // this logic is getting executed before if we define Before after if we define After and both before and after if we define Around
 //        System.out.println("Login Time");
@@ -27,14 +33,15 @@ public class TimeMonitorAspect {
             joinPoint.proceed();
         } catch (Throwable e) {
 
-            System.out.println("Something went wrong during the execution");
+            logger.error("Something went wrong during execution", e); //
+
 
         } finally {
             long end = System.currentTimeMillis(); // end
 
             long totalExecutionTime = end - start ;
 
-            System.out.println("Total Time of Execution Is " + totalExecutionTime + "ms..");
+            logger.info("Total Execution Time is {} ms", totalExecutionTime);
         }
 
 
